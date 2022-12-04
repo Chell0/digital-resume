67139997352b5fcef8dffd1d6cd38e30f442d767
Chell0 <machelgabriel@gmail.com>
2022-12-04 20:54:31 +0300

    ch: Add column to the Hero section

--- app.py
+++ app.py
@@ -41,3 +41,11 @@ with open(css_file) as f:
 with open(resume_file, "rb") as doc_file:
     DOCbyte = doc_file.read()
 profile_pic = Image.open(profile_pic)
+
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.image(profile_pic, width=230)
+    
+
